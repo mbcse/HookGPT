e5f57e4a1b015f505378fe449a0f540e19b09e33
mbcse <mbcse50@gmail.com>
2025-03-18 19:15:20 +0530

    Added openapi

---
+++ AgentBackend/src/api-docs/openAPIDocumentGenerator.ts
@@ -0,0 +1,21 @@
+import { OpenAPIRegistry, OpenApiGeneratorV3 } from "@asteasolutions/zod-to-openapi";
+
+import { healthCheckRegistry } from "@/api/healthCheck/healthCheckRouter";
+import { hooksRegistry } from "@/api/hooks/hooksRouter";
+
+export function generateOpenAPIDocument() {
+  const registry = new OpenAPIRegistry([healthCheckRegistry, hooksRegistry]);
+  const generator = new OpenApiGeneratorV3(registry.definitions);
+
+  return generator.generateDocument({
+    openapi: "3.0.0",
+    info: {
+      version: "1.0.0",
+      title: "Swagger API",
+    },
+    externalDocs: {
+      description: "View the raw OpenAPI Specification in JSON format",
+      url: "/swagger.json",
+    },
+  });
+}
