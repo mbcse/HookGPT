c29dd54209ca23827d4ffacac502c809d903d5ef
mbcse <mbcse50@gmail.com>
2025-03-27 15:15:20 +0530

    url fix env

--- AgentBackend/src/common/utils/envConfig.ts
+++ AgentBackend/src/common/utils/envConfig.ts
@@ -7,7 +7,9 @@ export const env = cleanEnv(process.env, {
   NODE_ENV: str({ devDefault: testOnly("test"), choices: ["development", "production", "test"] }),
   HOST: host({ devDefault: testOnly("localhost") }),
   PORT: port({ devDefault: testOnly(3000) }),
-  CORS_ORIGIN: str({ devDefault: testOnly("http://localhost:3001") }),
+  CORS_ORIGIN: str({
+    devDefault: testOnly("http://localhost:3001") || testOnly("http://localhost:3000") || testOnly("https://hookgpt.onrender.com"),
+  }),
   COMMON_RATE_LIMIT_MAX_REQUESTS: num({ devDefault: testOnly(1000) }),
   COMMON_RATE_LIMIT_WINDOW_MS: num({ devDefault: testOnly(1000) }),
   OPENAI_API_KEY: str(),
