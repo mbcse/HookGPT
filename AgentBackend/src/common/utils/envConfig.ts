7f590bae5126320c36b47423df6d69646089f606
mbcse <mbcse50@gmail.com>
2025-03-27 15:15:20 +0530

    url fix

--- AgentBackend/src/common/utils/envConfig.ts
+++ AgentBackend/src/common/utils/envConfig.ts
@@ -8,7 +8,7 @@ export const env = cleanEnv(process.env, {
   HOST: host({ devDefault: testOnly("localhost") }),
   PORT: port({ devDefault: testOnly(3000) }),
   CORS_ORIGIN: str({
-    devDefault: testOnly("http://localhost:3001") || testOnly("http://localhost:3000") || testOnly("https://hookgpt.onrender.com"),
+    devDefault: testOnly("http://localhost:3001") || testOnly("http://localhost:3000") || testOnly("https://hookgpt.onrender.com") || testOnly("https://hook-gpt.vercel.app"),
   }),
   COMMON_RATE_LIMIT_MAX_REQUESTS: num({ devDefault: testOnly(1000) }),
   COMMON_RATE_LIMIT_WINDOW_MS: num({ devDefault: testOnly(1000) }),
