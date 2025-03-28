18510630670e47a0ff03e2032cf43ce9a253cfde
mbcse <mbcse50@gmail.com>
2025-03-28 18:15:20 +0530

    fix cors

--- AgentBackend/src/server.ts
+++ AgentBackend/src/server.ts
@@ -17,10 +17,18 @@ const app: Express = express();
 // Set the application to trust the reverse proxy
 app.set("trust proxy", true);
 
+const originArray = env.CORS_ORIGIN.split(",");
+
 // Middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({ origin: env.CORS_ORIGIN, credentials: true }));
+app.use(cors({ origin:function (origin, callback) {
+    if (!origin || originArray.includes(origin.trim())) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  }, credentials: true }));
 app.use(helmet());
 app.use(rateLimiter);
 
